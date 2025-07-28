f2d9892a538f36a776a7aa395da1b5d720379e77
willtheliving <messagewillh@gmail.com>
2025-07-27 23:52:07 -0700

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,6 @@
-// See https://tailwindcss.com/docs/using-with/preprocessors for more details.
-// This configuration tells PostCSS to use Tailwind CSS and Autoprefixer.
-
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {},   // must match the package we just added
   },
 };
