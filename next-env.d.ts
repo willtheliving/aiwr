183f0dc03f92555c6314a19a0f9379b4ab32dde9
willtheliving <messagewillh@gmail.com>
2025-10-04 15:48:39 -0700

    Clarify lockfile tracking in gitignore

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited directly by developers.
-// It ensures TypeScript picks up the correct types for Next.js and its image component.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
